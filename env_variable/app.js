3c81aa08a0bdea48f29033324941662841cc5023
dipjoy-paul <antikpaul1994@gmail.com>
2023-08-01 19:22:00 +0530

    adding env files

---
+++ env_variable/app.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+
+
+
+module.exports = app;
