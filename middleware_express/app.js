c40e014f51d6f0384268febb2d4b3b6b7814e23c
dipjoy-paul <antikpaul1994@gmail.com>
2023-08-02 19:56:53 +0530

    adding middleware

---
+++ middleware_express/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+
+const myMiddleware = (req, res, next) => {
+console.log("Middleware function called");
+req.requestTime = new Date(Date.now());
+next();
+};
+
+
+
+app.use(myMiddleware);
+
+// app.use((req, res, next)=>{
+//   res.send("404 bad request");
+// });
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
+
+
+app.get("/",  (req, res) => {
+  console.log("i am home." + req.requestTime);  
+  res.send("<h1>This is home router</h1>");
+});
+
+app.get("/about",  (req, res) => {
+  console.log("i am about." + req.requestTime);
+  res.send("<h1>This is about router</h1>");
+});
+
+
+
+module.exports = app;
